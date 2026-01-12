a934e598f73fb7cd2504de3d80573bf5a3dbe38c
sunkolin <sunkolin@qq.com>
2026-01-12 09:48:20 +0800

    fix

---
+++ src/main/java/com/starfish/test/config/MyBatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.starfish.test.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatisPlusConfig
+ *
+ * @author sunkolin
+ * @version 1.0.0
+ * @since 2025-04-29
+ */
+@Configuration
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+
+}
