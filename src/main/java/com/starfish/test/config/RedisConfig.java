a4b7ed21fa9e4deac5b050c6b8f3e4330a6625f9
sunkolin <sunkolin@qq.com>
2026-01-14 14:50:42 +0800

    fix

--- src/main/java/com/starfish/test/config/RedisConfig.java
+++ src/main/java/com/starfish/test/config/RedisConfig.java
@@ -1,12 +1,14 @@
 package com.starfish.test.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import tools.jackson.databind.ObjectMapper;
 
 /**
  * RedisConfig
@@ -33,7 +35,14 @@ public class RedisConfig {
         // 3.1 字符串序列化器（用于key的序列化，推荐）
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         // 3.2 JSON序列化器（用于value的序列化，支持复杂Java对象，推荐）
-        GenericJacksonJsonRedisSerializer jsonRedisSerializer = new GenericJacksonJsonRedisSerializer(new ObjectMapper());
+//        GenericJacksonJsonRedisSerializer jsonRedisSerializer = new GenericJacksonJsonRedisSerializer(new ObjectMapper());
+//        GenericJacksonJsonRedisSerializer jsonRedisSerializer =  GenericJacksonJsonRedisSerializer.builder().build();
+//        JacksonJsonRedisSerializer jsonRedisSerializer = new JacksonJsonRedisSerializer(Object.class);
+        Jackson2JsonRedisSerializer jsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jsonRedisSerializer.setObjectMapper(objectMapper);
 
         // 4. 为不同类型的数据设置对应的序列化器
         redisTemplate.setKeySerializer(stringRedisSerializer); // key 序列化
