ef2a0e899e07ace486aeb6c2fef15800cb811da5
sunkolin <sunkolin@qq.com>
2026-03-16 11:13:08 +0800

    测试minio

--- src/main/java/com/starfish/test/controller/TestController.java
+++ src/main/java/com/starfish/test/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.starfish.test.controller;
 
+import com.starfish.common.storage.minio.MinioService;
 import com.starfish.core.annotation.RequireLogin;
 import com.starfish.core.model.Result;
 import com.starfish.test.entity.UserEntity;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Enumeration;
@@ -20,10 +23,16 @@ import java.util.Enumeration;
 @RestController
 public class TestController {
 
+    @Resource
+    private MinioService minioService;
+
     @RequireLogin(false)
     @RequestMapping(value = {"/api/test/testXForwardedFor"}, method = {RequestMethod.GET, RequestMethod.POST})
     public Result<String> alive(HttpServletRequest request) {
         Enumeration<String> names = request.getHeaderNames();
+        HttpHeaders fileHeaders = new HttpHeaders();
+
+        fileHeaders.setContentDispositionFormData("file", "fileName");
         log.info("{}", names);
         return Result.success("alive");
     }
@@ -34,6 +43,15 @@ public class TestController {
         Enumeration<String> names = request.getHeaderNames();
         log.info("{}", names);
         return Result.success("alive");
+    }
+
+    @RequireLogin(false)
+    @RequestMapping(value = {"/api/minio/createBucket"}, method = {RequestMethod.POST})
+    public Result<String> createBucket() {
+
+        minioService.createBucket("test");
+
+        return Result.success();
     }
 
 }
