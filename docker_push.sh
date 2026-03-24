1a25c58c2e4df4aa8bbdf883958d2a1ee247d9f7
sunkolin <sunkolin@qq.com>
2026-03-24 13:47:24 +0800

    fix

--- docker_push.sh
+++ docker_push.sh
@@ -5,8 +5,8 @@ source .env
 mvn clean install -Dmaven.test.skip=true
 
 # 打镜像
-sudo docker build -t sunkolin/$APP_NAME:$APP_VERSION -f Dockerfile .
-sudo docker tag sunkolin/$APP_NAME:$APP_VERSION sunkolin/$APP_NAME:latest
+sudo docker build -t sunkolin/${APP_NAME}:$APP_VERSION} -f Dockerfile .
+sudo docker tag sunkolin/${APP_NAME}:${APP_VERSION} sunkolin/${APP_NAME}:latest
 sudo docker login -u sunkolin -p 'XiaoZhi888+'
-sudo docker push sunkolin/$APP_NAME:$APP_VERSION
-sudo docker push sunkolin/$APP_NAME:latest
+sudo docker push sunkolin/${APP_NAME}:${APP_VERSION}
+sudo docker push sunkolin/${APP_NAME}:latest
--- docker_stop.sh
+++ docker_stop.sh
@@ -2,6 +2,6 @@ source .env
 
 echo "stop start"
 
-sudo docker ps -a -q --filter "application_name=$APP_NAME"|grep -q . && docker stop $APP_NAME && docker rm -fv $APP_NAME
+sudo docker ps -a -q --filter "application_name=${APP_NAME}"|grep -q . && docker stop ${APP_NAME} && docker rm -fv ${APP_NAME}
 
 echo "stop complete"
