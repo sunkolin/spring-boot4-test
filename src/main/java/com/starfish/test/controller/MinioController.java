866405e981b23e2316c7f5a166fd9a508d0603d4
sunkolin <sunkolin@qq.com>
2026-04-21 11:12:55 +0800

    增加执行项目的配置

--- src/main/java/com/starfish/test/controller/MinioController.java
+++ src/main/java/com/starfish/test/controller/MinioController.java
@@ -1,35 +1,35 @@
-package com.starfish.test.controller;
+// package com.starfish.test.controller;
 
-import com.starfish.common.storage.minio.MinioService;
-import com.starfish.core.annotation.RequireLogin;
-import com.starfish.core.model.Result;
-import jakarta.annotation.Resource;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+// import com.starfish.common.storage.minio.MinioService;
+// import com.starfish.core.annotation.RequireLogin;
+// import com.starfish.core.model.Result;
+// import jakarta.annotation.Resource;
+// import lombok.extern.slf4j.Slf4j;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RestController;
 
-/**
- * MinioController
- *
- * @author sunkolin
- * @version 1.0.0
- * @since 2020-09-13
- */
-@Slf4j
-@RestController
-public class MinioController {
+// /**
+// * MinioController
+// *
+// * @author sunkolin
+// * @version 1.0.0
+// * @since 2020-09-13
+// */
+// @Slf4j
+// @RestController
+// public class MinioController {
 
-    @Resource
-    private MinioService minioService;
+// @Resource
+// private MinioService minioService;
 
-    @RequireLogin(false)
-    @RequestMapping(value = {"/api/minio/createBucket"}, method = {RequestMethod.POST})
-    public Result<String> createBucket() {
+// @RequireLogin(false)
+// @RequestMapping(value = {"/api/minio/createBucket"}, method = {RequestMethod.POST})
+// public Result<String> createBucket() {
 
-        minioService.createBucket("test");
+// minioService.createBucket("test");
 
-        return Result.success();
-    }
+// return Result.success();
+// }
 
-}
+// }
